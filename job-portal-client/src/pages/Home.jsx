2ebfa892f61b1f690d4a7f7c667c6edc614f1b5c
karan singh D <ceitkaransingh2026@gmail.com>
2024-09-07 23:05:54 +0530

    Added searchbar job search

--- job-portal-client/src/pages/Home.jsx
+++ job-portal-client/src/pages/Home.jsx
@@ -1,9 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Banners from '../Components/Banners'
 
 const Home = () => {
+  const [selectedCategory,setselectedCategory]=useState(null);
+  const [jobs,setjobs]=useState([]);
   const [query, setQuery] = useState("");
 
+  useEffect(()=>{
+    fetch("jobs.json").then(res=> res.json()).then(data=> {
+      
+      setjobs(data)
+    })
+  },[])
+// console.log(job);
+
+  const filtereditem=jobs.filter((job)=>job.jobTitle.toLowerCase().indexOf(query.toLowerCase())!==-1);
+  
+  console.log(filtereditem);
+    
   const handleQueryChange = (e) => {
     setQuery(e.target.value);
     console.log(e.target.value); 
